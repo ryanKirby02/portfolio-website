11039fc8e00eab5056cdf9578abd696beed8c998
RyanCantCode <kirbyryan36@gmail.com>
2021-03-24 11:31:14 -0400

    added the Services Section

---
+++ src/components/ServicesSection.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import clock from '../img/clock.svg';
+import diaphragm from '../img/diaphragm.svg';
+import money from '../img/money.svg';
+import teamwork from '../img/teamwork.svg';
+import home2 from '../img/home2.png'
+
+const ServicesSection = () => {
+  return (
+    <div className='services'>
+      <div className='description'>
+        <h2>
+          High <span>Quality</span> services
+        </h2>
+        <div className='cards'>
+          <div className='card'>
+            <div className='icon'>
+                <img src={clock} alt="Clock Icon"/>
+                <h3>Efficient</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className='card'>
+            <div className='icon'>
+                <img src={teamwork} alt="TeamWork Icon"/>
+                <h3>Teamwork</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className='card'>
+            <div className='icon'>
+                <img src={diaphragm} alt="Diaphragm Icon"/>
+                <h3>Diaphragm</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className='card'>
+            <div className='icon'>
+                <img src={money} alt="Cash Icon"/>
+                <h3>Affordable</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+      </div>
+      <div className="image">
+          <img src={home2} alt="Cool camera pic"/>
+      </div>
+    </div>
+  );
+};
+
+export default ServicesSection;
